bf594d26c3fbcaed35cc77df0c778449b73636c1
MARUF AHMAD <m.ahmad2005@gmail.com>
2025-05-11 03:02:27 -0600

    update 6

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const pool = mysql.createPool({
 });
 
 // Main route
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   const { make, model, year, page = 1 } = req.query;
   const limit = 20;
   const offset = (page - 1) * limit;
@@ -88,7 +88,13 @@ app.get('/', (req, res) => {
       });
     });
   });
+}); */
+
+// Simple ping to verify server is alive
+app.get('/', (req, res) => {
+  res.send('✅ Main Page is running!');
 });
+
 
 // Simple ping to verify server is alive
 app.get('/ping', (req, res) => {
